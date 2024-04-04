d9576181931c597514aac77c000c968295f407ce
Aziz Kabia <158849915+aakabia@users.noreply.github.com>
2024-04-04 18:32:52 -0400

    render taks to screen

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,145 +1,155 @@
 // Retrieve tasks and nextId from localStorage
-let taskList = JSON.parse(localStorage.getItem("tasks"));
-let nextId = JSON.parse(localStorage.getItem("nextId"));
-let inputTaskTitle = document.querySelector("#inputTaskTitle")
-let inputDate = document.querySelector("#inputDate")
-let inputTask = document.querySelector("#inputTask")
-let addTask = document.querySelector("#addTask")
-let formModal = document.querySelector("#formModal")
+const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+const nextId = JSON.parse(localStorage.getItem("nextId"));
+let inputTaskTitle = document.querySelector("#inputTaskTitle");
+let inputDate = document.querySelector("#inputDate");
+let inputTask = document.querySelector("#inputTask");
+let addTask = document.querySelector("#addTask");
+let formModal = document.querySelector("#formModal");
+let toDoCards = document.querySelector("#todo-cards");
+flatpickr("#inputDate", {});
 
-
-
-
+// Above, I picked multiple itmes from my html and also made task list a empty array to use later. 
+// Also, i used the flatpickr API instead of dayjs to display the date. 
 
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-    const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let taskID = '';
-    for (let i = 0; i < 10; i++) {
-        taskID += characters.charAt(Math.floor(Math.random() * characters.length));
-    
-    }
-    return taskID;
-    
+  const characters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let taskID = "";
+  for (let i = 0; i < 10; i++) {
+    taskID += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return taskID;
+  
 
-};
+  // above, I created a id for each task card and assigned this id in createtaskcard line 48.
+}
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-    for (task in taskList){
-   let sectionEL = document.createElement("section");
-    let divHEL = document.createElement("div");
-   let divBEL = document.createElement("div");
-   let divh5 = document.createElement("h6")
-   let divh6 = document.createElement("h6")
-   let divP = document.createElement("p");
-    let deleteBttn = document.createElement("button");
+  
+  const sectionEL = document.createElement("section");
+  const divHEL = document.createElement("div");
+  const divBEL = document.createElement("div");
+  const divh5 = document.createElement("h6");
+  const divh6 = document.createElement("h6");
+  const divP = document.createElement("p");
+  const deleteBttn = document.createElement("button");
 
-    sectionEL.setAttribute("class", "card text-center")
-    divHEL.setAttribute("class", "card-header")
-    divBEL.setAttribute("class", "card-body" )
-    divh5.setAttribute("class", "card-title")
-    divh6.setAttribute("class", "card-title")
-    divP.setAttribute("class", "card-text" )
-    deleteBttn.setAttribute("class", "btn btn-primary")
+  sectionEL.setAttribute("class", "card text-center space");
+  divHEL.setAttribute("class", "card-header");
+  divBEL.setAttribute("class", "card-body");
+  divh5.setAttribute("class", "card-title");
+  divh6.setAttribute("class", "card-title");
+  divP.setAttribute("class", "card-text");
+  deleteBttn.setAttribute("class", "btn btn-primary");
 
+  sectionEL.dataset.taskId = task.id;
 
-    
+  toDoCards.appendChild(sectionEL);
+  sectionEL.appendChild(divHEL);
+  sectionEL.appendChild(divBEL);
+  divBEL.appendChild(divh5);
+  divBEL.appendChild(divh6);
+  divBEL.appendChild(divP);
+  divBEL.appendChild(deleteBttn);
 
-
-
-
-
-
-
-    }
+  
+  // Above, I created a task card using a bootstrap layout. 
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
+    
+    let task = taskList;
+    console.log(task)
+    task.forEach(createTaskCard)
 
+   // Above, I used a for each to itterate over task  
 }
+
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
-    event.preventDefault()
-    
+function handleAddTask(event) {
+  event.preventDefault();
+  
 
-    let taskTitleValue = inputTaskTitle.value.trim();
-    let dateValue = inputDate.value;
-    let taskValue = inputTask.value.trim();
+  let taskTitleValue = inputTaskTitle.value.trim();
+  let dateValue = inputDate.value;
+  let taskValue = inputTask.value.trim();
 
-    let userTask = JSON.parse(localStorage.getItem("tasks")) ||[];
+  // Above I made vribale values and added a trim 
 
-    let userInfo = {
-       title: taskTitleValue,
-       dateDue: dateValue,
-       task: taskValue
-    
-      
-    };
+  let userInfo = {
+    title: taskTitleValue,
+    dateDue: dateValue,
+    task: taskValue,
+    id: generateTaskId(),
+  };
 
-      
-    if (taskTitleValue === "") {
-        alert("Please fill in Title!");
-        return false;
-      } else if (dateValue === "") {
-       
-        alert("Please fill in Date!");
-        return false;
-      } else if (taskValue === "") {
-        
-        alert("Please fill in Task!");
-        return false;
-      } else {
-      
-    };
+  if (taskTitleValue === "") {
+    alert("Please fill in Title!");
+    return false;
+  } else if (dateValue === "") {
+    alert("Please fill in Date!");
+    return false;
+  } else if (taskValue === "") {
+    alert("Please fill in Task!");
+    return false;
+  } else {
+  }
 
-    
-    userTask.push(userInfo);
+  
+    taskList.push(userInfo);
+  
+    // Above I created a object for my modal and made sure the inputs arent empty 
 
-    
+  let userTaskSerialized = JSON.stringify(taskList);
 
+  
+  localStorage.setItem("tasks", userTaskSerialized);
 
+  // Above, I set the seralized the task and saved them to local storage 
 
-    let userTaskSerialized = JSON.stringify(userTask);
+  $(formModal).modal("hide");
 
-    localStorage.setItem("tasks", userTaskSerialized);
+  inputTaskTitle.value = "";
+  inputDate.value = "";
+  inputTask.value = "";
 
-    $(formModal).modal('hide');
+ // Above I hide the modal and cleared it 
 
-    inputTaskTitle.value='';
-    inputDate.value='';
-    inputTask.value='';
+  renderTaskList();
 
- 
-
-
-
-
+  window.location.reload();
+  // important to refresh the scrren when dealing with local storage on same page. 
+  // use window.location.reload()
+  // This stops reitteration 
+  // Stuck on this problem for hours !!!
 }
+
+
+
+
+
+
+
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
-
-}
+function handleDeleteTask(event) {}
 
 // Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {
-
-}
+function handleDrop(event, ui) {}
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+  addTask.addEventListener("click", handleAddTask); 
+    renderTaskList();
 
+   
 });
 
 
-
-addTask.addEventListener("click", handleAddTask )
-
-
-
-
-
+ 
